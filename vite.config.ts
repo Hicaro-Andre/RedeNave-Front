cc8a7b8dfe0346163e476ebef2a7a1cb033b5934
Hicaro Andre <hicaroandre@hotmail.com>
2026-01-17 22:05:50 -0300

    feat: adjuste preview v1

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "dist",
+    outDir: "dist", // pasta de build
   },
   preview: {
     port: 4173,
     allowedHosts: [
-      "convertible-ribbon-tell-councils.trycloudflare.com",
+      'convertible-ribbon-tell-councils.trycloudflare.com'
     ],
   },
+  base: '/', // garante que todas as rotas apontem para /
 });
